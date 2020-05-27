c57d0e87849b056dc8ef871603d7214109e52b78
Simon Fraipont <simon.fraipont@gmail.com>
2020-05-27 16:03:07 +0200

    add correct creation of user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,9 @@ const User = require.main.require('./db/models/user');
 
 const debug = require('debug')('t-issue:userController');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 /*
   @desc Get all users
@@ -30,14 +33,37 @@ exports.new = (req, res, next) => {
   @desc Create user
   @route POST /api/users
 */
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.json(errors);
   }
 
-  return res.json('hello from user create');
+  try {
+    const password = await bcrypt.hash(req.body.password, saltRounds);
+    req.body.password = password;
+  } catch (err) {
+    next(err);
+  }
+
+  if (!req.body.roleId) {
+    req.body.roleId = 1;
+  }
+
+  try {
+    const result = await User.create(
+      req.body.first_name,
+      req.body.last_name,
+      req.body.email,
+      req.body.password,
+      req.body.roleId
+    );
+
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
 /*
@@ -65,8 +91,21 @@ exports.show = async (req, res, next) => {
   @desc Get user edit form
   @route GET /api/users/edit
 */
-exports.edit = (req, res, next) => {
-  return res.json('hello from user edit');
+exports.edit = async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.params.id);
+
+    if (currentUser == null) {
+      const error = new Error('User not found');
+      error.status = 404;
+
+      next(error);
+    }
+
+    return res.json(currentUser);
+  } catch (err) {
+    next(err);
+  }
 };
 
 /*
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,8 +2,36 @@ require('dotenv').config();
 const db = require('../db');
 const debug = require('debug')('t-issue:userModel');
 
-//function personFactory( firstName, lastName, email, password, roleId ) {
-//}
+exports.userFactory = function (firstName, lastName, email, password, roleId) {
+  return {
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    password,
+    role_id: roleId
+  };
+};
+
+exports.create = async (firstName, lastName, email, password, roleId) => {
+  const query = {
+    text: 'INSERT INTO "user"(first_name, last_name, email, password, role_id) VALUES($1, $2, $3, $4, $5) RETURNING user_id',
+    values: [
+      firstName,
+      lastName,
+      email,
+      password,
+      roleId
+    ]
+  };
+
+  try {
+    const result = await db.one(query);
+
+    return result;
+  } catch (err) {
+    debug('ERROR:', err);
+  }
+};
 
 exports.findAll = async () => {
   const query = 'SELECT first_name, last_name, email, password, r.name as role FROM "user" u INNER JOIN role r ON u.role_id = r.role_id';
